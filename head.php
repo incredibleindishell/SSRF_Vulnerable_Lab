9da7dc1688c8161c8b8cd8f6df9efe9c1aa06dac
Incredible <manish.1046@gmail.com>
2019-09-22 19:47:44 +0530

    Create head.php

---
+++ head.php
@@ -0,0 +1,163 @@
+<?php
+
+
+$head = '
+<html>
+<head>
+<link href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLfLXmLeMSTt0jOXREfgvdp8IYWnE9_t49PpAiJNvwHTqnKkL4" rel="icon" type="image/x-icon"/>
+</script>
+<title>--==[[IndiShell Lab]]==--</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<STYLE>
+body {
+	background: url(images/head.jpg);
+	background-size: 100% 700px;
+    background-repeat: no-repeat;
+font-family: Tahoma;
+color: white;
+
+}
+.side-pan {
+   margin: 0;
+   border:0px;
+   
+   width:200px;
+   padding: 5px 23px;
+   margin:0px;
+   -webkit-border-radius: 0px;
+   -moz-border-radius: 0px;
+   border-radius: 0px;
+   border-bottom: 1px solid black;
+   color: white;
+   font-size: 20px;
+   font-family: Georgia, serif;
+   text-decoration: none;
+   vertical-align: left;
+   align:left;
+   }
+   div#left {
+    width: 100%;
+    height: 50px;
+    float: left;
+	}
+div#right {
+    margin-left: 20%;
+    height: 50px;
+	color: white;
+    font-size: 20px;
+    font-family: Georgia, serif;
+	}
+.main div {
+  float: left;
+  clear: none; 
+	}
+
+input {
+border			: solid 2px ;
+border-color		: black;
+BACKGROUND-COLOR: #444444;
+font: 8pt Verdana;
+color: white;
+}
+submit {
+BORDER:  buttonhighlight 2px outset;
+BACKGROUND-COLOR: Black;
+width: 30%;
+color: #FFF;
+}
+#t input[type=\'submit\']{
+	COLOR: White;
+	border:none;
+	BACKGROUND-COLOR: black;
+}
+#t input[type=\'submit\']:hover {
+	
+	BACKGROUND-COLOR: #ff9933;
+	color: black;
+	
+}
+tr {
+BORDER: dashed 1px #333;
+color: #FFF;
+}
+td {
+BORDER: dashed 0px ;
+}
+.table1 {
+BORDER: 0px Black;
+BACKGROUND-COLOR: Black;
+color: #FFF;
+}
+.td1 {
+BORDER: 0px;
+BORDER-COLOR: #333333;
+font: 7pt Verdana;
+color: Green;
+}
+.tr1 {
+BORDER: 0px;
+BORDER-COLOR: #333333;
+color: #FFF;
+}
+table {
+BORDER: dashed 2px #333;
+BORDER-COLOR: #333333;
+BACKGROUND-COLOR: #191919;;
+color: #FFF;
+}
+textarea {
+border			: dashed 2px #333;
+BACKGROUND-COLOR: Black;
+font: Fixedsys bold;
+color: #999;
+}
+A:link {
+border: 1px;
+	COLOR: red; TEXT-DECORATION: none
+}
+A:visited {
+	COLOR: red; TEXT-DECORATION: none
+}
+A:hover {
+	color: White; TEXT-DECORATION: none
+}
+A:active {
+	color: white; TEXT-DECORATION: none
+}
+</STYLE>
+<script type="text/javascript">
+<!--
+    function lhook(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == \'block\')
+          e.style.display = \'none\';
+       else
+          e.style.display = \'block\';
+    }
+//-->
+</script>
+'; 
+
+echo $head ;
+
+echo '
+<table width="100%" cellspacing="0" cellpadding="0" class="tb1" style="opacity: 0.7;border:0px;" >
+			
+       <td width="100%" align=center valign="top" rowspan="1">
+	   <font color=#ff9933 size="-2"> 
+        ##########################################</font><font color=white size="-2">#############################################</font><font color=green size="-2">#############################################</font><br>
+           <font color=#ff9933 size=5 face="comic sans ms"><b>--==[[ SSRF </font><font color=white size=5 face="comic sans ms"><b>  Vulnerable </font><font color=green size=5 face="comic sans ms"><b> Lab]]==--</font><br>
+		   <font color=#ff9933 size=5 face="comic sans ms"><b>--==[[</b> With Love</font><font color=white size=5 face="comic sans ms"><b> From  Team </font><font color=green size=5 face="comic sans ms">IndiShell<b>]]==--</font>
+		   <div class="hedr"> 
+        <td height="10" align="left" class="td1"></td></tr><tr><td 
+        width="100%" align="center" valign="top" rowspan="1">
+		<font color=#ff9933 size="-2"> 
+        ##########################################</font><font color=white size="-2">#############################################</font><font color=green size="-2">#############################################</font><br>
+		<font 
+        color="red" face="comic sans ms"size="1">
+        
+						
+          
+       </table> 
+'; 
+?>
