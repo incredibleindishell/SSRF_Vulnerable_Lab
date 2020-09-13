ed26246f1781ca5d66d657a6f0e68e45d817e26a
Incredible <manish.1046@gmail.com>
2020-09-13 19:29:11 +0530

    Update pdf_ssrf_wkhtmltopdf.php

--- pdf_ssrf_wkhtmltopdf.php
+++ pdf_ssrf_wkhtmltopdf.php
@@ -1,5 +1,146 @@
-<?php 
-include('head2.php');
+<?php
+
+echo '
+<html>
+<head>
+<link href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLfLXmLeMSTt0jOXREfgvdp8IYWnE9_t49PpAiJNvwHTqnKkL4" rel="icon" type="image/x-icon"/>
+</script>
+<title>--==[[IndiShell Lab]]==--</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<STYLE>
+body {
+	background: url(indishell.jpg);
+	background-size: 100% 650px;
+    background-repeat: no-repeat;
+	background-attachment: fixed;
+font-family: Tahoma;
+color: white;
+}
+.side-pan {
+   margin: 0;
+   border:0px;
+   
+   width:200px;
+   padding: 5px 23px;
+   margin:0px;
+   -webkit-border-radius: 0px;
+   -moz-border-radius: 0px;
+   border-radius: 0px;
+   border-bottom: 1px solid black;
+   color: white;
+   font-size: 20px;
+   font-family: Georgia, serif;
+   text-decoration: none;
+   vertical-align: left;
+   align:left;
+   }
+   div#left {
+    width: 100%;
+    height: 50px;
+    float: left;
+	}
+div#right {
+    margin-left: 20%;
+    height: 50px;
+	color: white;
+    font-size: 20px;
+    font-family: Georgia, serif;
+	}
+.main div {
+  float: left;
+  clear: none; 
+	}
+input {
+border			: solid 2px ;
+border-color		: black;
+BACKGROUND-COLOR: #444444;
+font: 8pt Verdana;
+color: white;
+}
+submit {
+BORDER:  buttonhighlight 2px outset;
+BACKGROUND-COLOR: Black;
+width: 30%;
+color: #FFF;
+}
+#t input[type=\'submit\']{
+	COLOR: White;
+	border:none;
+	BACKGROUND-COLOR: black;
+}
+#t input[type=\'submit\']:hover {
+	
+	BACKGROUND-COLOR: #ff9933;
+	color: black;
+	
+}
+tr {
+BORDER: dashed 1px #333;
+color: #FFF;
+}
+td {
+BORDER: dashed 0px ;
+}
+
+table {
+BORDER: dashed 2px #333;
+BORDER-COLOR: #333333;
+BACKGROUND-COLOR: #191919;;
+color: #FFF;
+}
+textarea {
+border			: dashed 2px #333;
+BACKGROUND-COLOR: Black;
+font: Fixedsys bold;
+color: #999;
+}
+A:link {
+border: 1px;
+	COLOR: red; TEXT-DECORATION: none
+}
+A:visited {
+	COLOR: red; TEXT-DECORATION: none
+}
+A:hover {
+	color: White; TEXT-DECORATION: none
+}
+A:active {
+	color: white; TEXT-DECORATION: none
+}
+
+</STYLE>
+<script type="text/javascript">
+<!--
+    function lhook(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == \'block\')
+          e.style.display = \'none\';
+       else
+          e.style.display = \'block\';
+    }
+//-->
+</script>
+
+<table width="100%" cellspacing="0" cellpadding="0" class="tb1" >
+			
+       <td width="100%" align=center valign="top" rowspan="1">
+           <font color=#ff9933 size=5 face="comic sans ms"><b>--==[[ WKHTMLtoPDF, HTML</font><font color=white size=5 face="comic sans ms"><b> to PDF Converter Server-side</font><font color=green size=5 face="comic sans ms"><b> request forgery ]]==--</font><br>
+			<font color=#ff9933 size=3 face="comic sans ms"><b>--==[[ With </font><font color=white size=3 face="comic sans ms"><b>Love From IndiShell</font><font color=green size=3 face="comic sans ms"><b> Crew]]==--</font>
+		   <div class="hedr"> 
+        <td height="10" align="left" class="td1"></td></tr><tr><td 
+        width="100%" align="center" valign="top" rowspan="1"><font 
+        color="red" face="comic sans ms"size="1"><b> 
+        <font color=#ff9933> 
+        ##########################################</font><font color=white>#############################################</font><font color=green>#############################################</font><br><font color=white>
+        -==[[Greetz to]]==--</font><br> <font color=#ff9933>Zero cool, code breaker ica, r0ot_devil, google_warrior, INX_r0ot, Darkwolf indishell, Baba ,Silent poison India, Magnum sniper, 3thicalnoob Indishell, cyber warrior, Hacker Fantastic and rest of TEAM INDISHELL<br>
+<font color=white>--==[[Love to]]==--</font><br># My Father, my Ex Teacher, Lovey, cold fire hacker, Mannu, ViKi, Incredible, Hardeep Singh, Ashu bhai ji, Rafay Baloch, Soldier Of God, Shafoon, Rehan Manzoor, almas malik, Bhuppi, Mohit, Ffe ^_^, Govind Singh, Shardhanand, Budhaoo, Don(Deepika kaushik) and D3 <br>
+        <b> 
+        <font color=#ff9933> 
+        ##########################################</font><font color=white>#############################################</font><font color=green>#############################################</font>
+						
+           </table>
+       </table> ';
+
 
 		echo '<div id="left"><div class="main"><table align=center  cellspacing="0" cellpadding="0" style="border-collapse: collapse;border:0px;">
 		<tr>
@@ -127,12 +268,12 @@ $all=fopen('sample.html','w');
 fwrite($all,$html);
 
 
-//$path_pdf_converter='C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'; remove the comment if you want to use it on Windows machine
-//passthru('"'.$path_pdf_converter.'" -T 0 -R 0 -B 0 -L 0 --orientation Portrait --page-size A4 sample.html output4.pdf'); remove the comment if you want to use it on Windows machine
+//$path_pdf_converter='C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'; /*remove the comment if you want to use it on Windows machine*/
+//passthru('"'.$path_pdf_converter.'" -T 0 -R 0 -B 0 -L 0 --orientation Portrait --page-size A4 sample.html output4.pdf'); /*remove the comment if you want to use it on Windows machine*/
  passthru('xvfb-run wkhtmltopdf -T 0 -R 0 -B 0 -L 0 --orientation Portrait --page-size A4 --quiet sample.html output4.pdf 2>&1'); //comment this line if you want to use code on Windows
  
  
- echo '<table width="35%" style=" border:0px;background-color: #191919; opacity: 0.6; padding: 20px;" >
+ echo '<table width="35%" style=" border:0px;background-color: #191919; opacity: 0.8; padding: 20px;" >
 			<form method=post>
 		<tr><td align=center  colspan="2">Hello '.htmlentities($data).', please download the generated PDF. Click <a href="output4.pdf">Here</a></td></tr></table>';
 }
